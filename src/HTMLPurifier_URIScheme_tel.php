e893f781b4e0300dc76fdb04bb45e3bb12f2b823
Rob Taylor <rob@uxblondon.com>
2014-11-06 21:30:31 +0000

    Added HTMLPurifier Tel Scheme

--- src/HTMLPurifier_URIScheme_tel.php
+++ src/HTMLPurifier_URIScheme_tel.php
@@ -2,14 +2,12 @@
 
 class HTMLPurifier_URIScheme_tel extends \HTMLPurifier_URIScheme
 {
-    public $default_port = null;
-
     public $browsable = true;
 
     public $may_omit_host = true;
 
     public function doValidate(&$uri, $config, $context)
     {
-        return true;
+        return preg_match('|(+)?(\d+)(x\d+)?|', $uri);
     }
 }
